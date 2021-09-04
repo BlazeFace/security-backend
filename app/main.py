64efef4dfaebec2262053e69cdf0ee4050f6eeee
Nate <nateb@vt.edu>
2021-09-04 12:08:21 -0400

    added cors

--- app/main.py
+++ app/main.py
@@ -1,10 +1,22 @@
 import uvicorn
 from fastapi import FastAPI
-
+from fastapi.middleware.cors import CORSMiddleware
 import mongo
 
 app = FastAPI()
 
+origins = [
+    "localhost",
+    "security.eastus2.azurecontainer.io"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/content/create/{cont_id}")
 async def create_content(cont_id):
