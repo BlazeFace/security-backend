006009d9d002e8899c661f4db9ba2860ffccccec
Nate <nateb@vt.edu>
2021-09-04 17:52:56 -0400

    added cors

--- app/main.py
+++ app/main.py
@@ -5,26 +5,22 @@ import mongo
 
 app = FastAPI()
 
+origins = [
+    "http://localhost:4000",
+    "http://localhost:80",
+    "http://localhost",
+    "http://localhost:8080",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 # Salt to your taste
-ALLOWED_ORIGINS = '*'    # or 'foo.com', etc.
 
-# handle CORS preflight requests
-@app.options('/{rest_of_path:path}')
-async def preflight_handler(request: Request, rest_of_path: str) -> Response:
-    response = Response()
-    response.headers['Access-Control-Allow-Origin'] = ALLOWED_ORIGINS
-    response.headers['Access-Control-Allow-Methods'] = 'POST, GET, DELETE, OPTIONS'
-    response.headers['Access-Control-Allow-Headers'] = 'Authorization, Content-Type, Access-Control-Allow-Origin'
-    return response
-
-# set CORS headers
-@app.middleware("http")
-async def add_CORS_header(request: Request, call_next):
-    response = await call_next(request)
-    response.headers['Access-Control-Allow-Origin'] = ALLOWED_ORIGINS
-    response.headers['Access-Control-Allow-Methods'] = 'POST, GET, DELETE, OPTIONS'
-    response.headers['Access-Control-Allow-Headers'] = 'Authorization, Content-Type'
-    return response
 
 @app.get("/content/create/{cont_id}")
 async def create_content(cont_id):
